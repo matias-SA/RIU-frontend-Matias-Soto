b0f57fffde548d6438a5c8cda24659135bde37d1
Matias Soto <maatias.martin@hotmail.com>
2025-03-01 12:57:21 -0300

    primer commit configuracion de router y assets agrego data.json creo dahsboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "dashboard",
+    loadComponent: () => import("./pages/dashboard/dashboard.component"),
+  },
+  {
+    path: "",
+    redirectTo: "dashboard",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    redirectTo: "dashboard",
+    pathMatch: "full",
+  },
+];
